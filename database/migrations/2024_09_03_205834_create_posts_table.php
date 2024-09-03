fb2733879d267dadb16fdd5d9876a301d3e1878a
1ndoryu <andoryyu@gmail.com>
2024-09-03 17:04:18 -0400

    *

--- database/migrations/2024_09_03_205834_create_posts_table.php
+++ database/migrations/2024_09_03_205834_create_posts_table.php
@@ -9,14 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
+    
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
-    }
-
+    }    
     /**
      * Reverse the migrations.
      */
