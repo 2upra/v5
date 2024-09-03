b042cd5141d71fddca9030187935837e87b5182d
1ndoryu <andoryyu@gmail.com>
2024-09-03 17:45:11 -0400

    *

--- database/migrations/2024_09_03_214309_modify_posts_table.php
+++ database/migrations/2024_09_03_214309_modify_posts_table.php
@@ -9,16 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Relación con usuarios
+            $table->string('title'); // Título de la publicación
+            $table->text('content'); // Contenido de la publicación
+            $table->timestamps();
+        });
     }
-
+     
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('posts');
     }
 };
