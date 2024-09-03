b6286076df83f634d1fb3c063bcd1621d4464aad
1ndoryu <andoryyu@gmail.com>
2024-09-03 17:08:00 -0400

    *

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    protected $fillable = ['title', 'body', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
