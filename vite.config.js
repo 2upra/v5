257903fde20d6202f6cc0d09207f90e7e70dc64d
1ndoryu <andoryyu@gmail.com>
2024-09-03 16:34:40 -0400

    *

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5175,
+        https: {
+            key: fs.readFileSync('/etc/letsencrypt/live/2upra.com/privkey.pem'),
+            cert: fs.readFileSync('/etc/letsencrypt/live/2upra.com/fullchain.pem'),
+        },
+        hmr: {
+            host: '2upra.com',
+            protocol: 'wss', // Utiliza WebSocket seguro
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
-            ssr: 'resources/js/ssr.jsx',
             refresh: true,
         }),
         react(),
