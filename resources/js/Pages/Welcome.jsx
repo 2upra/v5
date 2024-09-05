06e5c74ff2878ad8dd738b81bf07fae91ed3d2e7
1ndoryu <andoryyu@gmail.com>
2024-09-05 11:34:05 -0400

    *

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -4,7 +4,7 @@ import {
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
-} from "@/components/ui/accordion";
+} from "@/components/accordion";
 
 export default function Welcome({ auth }) {
     return (
