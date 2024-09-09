721f39ac12b206c329038b3b82de4c73f160f3ac
1ndoryu <andoryyu@gmail.com>
2024-09-09 14:17:01 -0400

    *

--- resources/js/Components/formpost.jsx
+++ resources/js/Components/formpost.jsx
@@ -4,7 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { useState } from "react";
-import ButtonCheckbox from "@/components/ButtonCheckBox";
+import ButtonCheckbox from "@/Components/ButtonCheckBoxs";
 import { Button } from "@/components/button";
 import Waveform from "@/components/Wave";
 import {
